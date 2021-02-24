5f07a88b1e27c6ecb2f6ef520108815037b291b3
Murilo S. Rodrigues Jr <murilo.cc.uepb@gmail.com>
2021-02-23 21:10:09 -0300

    implementação Interface ClienteDAO

--- src/dao/ClientesDAO.java
+++ src/dao/ClientesDAO.java
@@ -1,5 +1,17 @@
 package dao;
+import java.util.ArrayList;
+
+import bean.Cliente;
 
 public interface ClientesDAO {
 
+	public void addCliente(Cliente cliente);
+	
+	public void getClienteById(int clienteId);
+	
+	public void updateClienteById (Cliente cliente);
+	
+	public void deleteClienteById (int clienteId);
+	
+	public ArrayList<Cliente> findAllCliente();
 }
